45be9c1f47a09bc333f5c2c4df9fedd4367919a9
Fernando Teles <fernandotalves@gmail.com>
2021-08-27 13:36:54 -0300

    add event model and tests

---
+++ application/the_eye/tests/event_model_tests.py
@@ -0,0 +1,25 @@
+import uuid
+from django.test import TransactionTestCase
+from the_eye.models import Event
+
+
+class EventModelTestCase(TransactionTestCase):
+    """ Defines the test suite for Event model."""
+
+    def test_can_create_event_model(self):
+        """ Tests the event model creation"""
+
+        self.event = Event(
+            session_id=uuid.uuid4(),
+            category = "page interaction",
+            name="pageview",
+            data = {},
+            timestamp = "2021-07-27 04:43:10.243860"
+        )
+
+        old_count = Event.objects.count()
+
+        self.event.save()
+        new_count = Event.objects.count()
+
+        self.assertEqual(old_count + 1, new_count)
